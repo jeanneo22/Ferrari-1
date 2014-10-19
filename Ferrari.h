75757decce1de38b6a75e209473e89bc59f7d954
FelipeQuevici <Felipe_quevici@hotmail.com>
2014-10-19 11:37:13 -0300

    Requisitos

--- Ferrari.h
+++ Ferrari.h
@@ -1,6 +1,33 @@
 #ifndef FERRARI_H
 #define FERRARI_H
 
+
+/*
+Comando: Dentre outras funcionalidades propospostas por você, deve ter armazenado o codigo do modelo e sua velocidade maxima.
+ Deve tambem armazenar se o motor turbo está ligado, marcha ativa e velocidade atual.
+
+Requisitos funcionais: Todos os atributos e funções membros devem estar relacionados a classe 
+ * 1. Pelo menos 4 atributos. OK
+ * 2. pelo menos 4 funções membros sem incluir get e set  OK
+ * 3. todos os atributos devem ser inicializados  OK
+ * 4. tres construtores, incluindo um construtor de copia e construtor com paramentros default  OK
+ * 5. deve ter um atributo string  OK
+ * 6. um atributo static  OK
+ * 7. um atributo const static OK
+ * 8. dois metodos constante OK
+ * 9. um array OK
+ * 10. uma função inline OK
+ * 11. metodo com passagem por referencia usando ponteiro  OK
+ * 12. metodo static - deve ser chamado no main  OK
+ * 13. composição com a classe data. OK
+
+ * Usar a classe vetor OK
+ * Alocação dinamica de memoria OK
+ * sobrecarga do operador == OK
+ * sobrecarga do operador << OK
+ 
+ * Composição com mais duas classes OK
+*/
 #include <string>
 using std::string;
 
