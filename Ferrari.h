4064e6ee925737c677ab1e8ed06fb7006a7ab287
FelipeQuevici <Felipe_Quevici@hotmail.com>
2014-10-21 13:10:58 -0300

    Comentarios
    Comentarios no codigo.

--- Ferrari.h
+++ Ferrari.h
@@ -63,28 +63,66 @@ private:
 	Pessoa dono;
 	vector<Multa> multas;
 public:
+	/*
+	 * Sobrecarga dos operadores.
+	 */ 
 	bool operator== (const Ferrari &right);
 	void operator= (const Ferrari &right);
 
 	~Ferrari();
 
+	/* 
+	 * Construtores
+	 */ 
 	Ferrari(const string& modelo = "", float velocidadeMaxima = 300, int marchaTotal = 6, const Data& dataFabricacao = Data(25,9,2014), const Pessoa& dono = Pessoa());
 	Ferrari(const Ferrari& copia);
 	Ferrari(const Data& dataFabricacao,const Pessoa& dono, bool isF599XX = false);
 	
+	/*
+	 * Fazer o update do software das ferraris. 
+	 */
 	static void updateSoftware(string versao);
 	
+	/*
+	 * Trocar marcha
+	 */ 
 	void mudarMarcha(int marcha);
+	
+	/*
+	 * Acelerar o carro.
+	 */ 
 	void acelerar(float quantidade);
+	
+	/*
+	 * Desacelerar o carro.
+	 */ 
 	void desacelerar();
+	
+	/*
+	 * Setar motor turbo para o valor oposto.
+	 */ 
 	inline void toggleMotorTurbo();
+	
+	/*
+	 * Botar uma multa no carro.
+	 */ 
 	void adicionarMulta(const Multa &multa);
+	
+	/*
+	 * Trocar dois carros de dono.
+	 */ 
 	static void trocarDonos(Ferrari * f1, Ferrari * f2);
 
+	/*
+	 * Funções para imprimir dados diversos.
+	 */ 
 	void imprimirVelocidade() const;
 	void imprimirDados() const;	
 	void imprimirMultas() const;
 
+	/*
+	 * Sets
+	 */ 
 	void setDataFabricacao(const Data& dataFabricacao);
 	void setMarchaAtiva(int marchaAtiva) ;
 	void setMarchaTotal(int marchaTotal) ;
@@ -95,7 +133,11 @@ public:
 	void setVelocidadeAtual(float velocidadeAtual) ;
 	void setVelocidadeMaxima(float velocidadeMaxima) ;
 	void setDono(const Pessoa& dono) ;
+	void setDinheiroPortaTreco(int valor, int indice);
 
+	/*
+	 * Gets
+	 */ 
 	const Data& getDataFabricacao() const ;
 	int getMarchaAtiva() const ;
 	int getMarchaTotal() const ;
@@ -107,9 +149,8 @@ public:
 	float getVelocidadeMaxima() const ;
 	int getQuantidadePortaTreco() const ;
 	Pessoa getDono() const;
-
 	int getDinheiroPortaTreco(int indice) const;
-	void setDinheiroPortaTreco(int valor, int indice);
+
 
 };
 #endif
